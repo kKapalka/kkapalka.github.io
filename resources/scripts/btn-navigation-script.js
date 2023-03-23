f8a66786fb6844f9b5a964634f3096092a3b4cd5
kKapalka <k.kapalka@hotmail.com>
2023-03-23 10:39:22 +0100

    fix buttons

--- resources/scripts/btn-navigation-script.js
+++ resources/scripts/btn-navigation-script.js
@@ -1,21 +1,82 @@
+
+
+function getMostVisible($elements) {
+    var element,
+        viewportHeight = $(window).height(),
+        max = 0;
+
+    $elements.each(function() {
+        var visiblePx = getVisibleHeightPx($(this), viewportHeight);
+
+        if (visiblePx > max) {
+            max = visiblePx;
+            element = this;
+        }
+    });
+
+    return $elements.filter(element);
+}
+
+function getVisibleHeightPx($element, viewportHeight) {
+    var rect = $element.get(0).getBoundingClientRect(),
+        height = rect.bottom - rect.top,
+        visible = {
+            top: rect.top >= 0 && rect.top < viewportHeight,
+            bottom: rect.bottom > 0 && rect.bottom < viewportHeight
+        },
+        visiblePx = 0;
+
+    if (visible.top && visible.bottom) {
+        // Whole element is visible
+        visiblePx = height;
+    } else if (visible.top) {
+        visiblePx = viewportHeight - rect.top;
+    } else if (visible.bottom) {
+        visiblePx = rect.bottom;
+    } else if (height > viewportHeight && rect.top < 0) {
+        var absTop = Math.abs(rect.top);
+
+        if (absTop < height) {
+            // Part of the element is visible
+            visiblePx = height - absTop;
+        }
+    }
+
+    return visiblePx;
+}
+
 $(document).ready(function() {
   // Hide the "up" button on page load
-  $("#btn-up").hide();
+  var largestSection = getMostVisible($("section"))
+  largestSection.addClass("active");
 
+  // Show or hide the "up" and "down" buttons
+  if (largestSection.is("#jumbotron")) {
+    $("#btn-up").hide();
+  } else {
+    $("#btn-up").show();
+  }
+  if (largestSection.is("#contact")) {
+    $("#btn-down").hide();
+  } else {
+    $("#btn-down").show();
+  }
   // Add click event listener to the "up" button
   $("#btn-up").click(function() {
     // Get the previous section
     var prevSection = $("section.active").prev();
-
+    var top = prevSection.offset().top
+    if ($("section.active").is("#jumbotron")) {
+      top = 0
+    }
     // Scroll to the previous section
     $("html, body").animate({
-      scrollTop: prevSection.offset().top
+      scrollTop: top
     }, 1000);
 
     // Update the active section
     $("section.active").removeClass("active");
     prevSection.addClass("active");
-
     // Show or hide the "up" and "down" buttons
     if (prevSection.is("#jumbotron")) {
       $("#btn-up").hide();
@@ -57,18 +118,18 @@ $(document).ready(function() {
     if (currentSection != null && !currentSection.hasClass("active")) {
       $("section.active").removeClass("active");
       currentSection.addClass("active");
-
-      // Show or hide the "up" and "down" buttons
-      if (currentSection.is("#jumbotron")) {
+      
+    }
+// Show or hide the "up" and "down" buttons
+      if($(window).scrollTop() === 0) {
         $("#btn-up").hide();
       } else {
         $("#btn-up").show();
       }
-      if (currentSection.is("#contact")) {
+      if($(window).scrollTop() + $(window).height() == $(document).height()) {
         $("#btn-down").hide();
       } else {
         $("#btn-down").show();
       }
-    }
   });
 });
